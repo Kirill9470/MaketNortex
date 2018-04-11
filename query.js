bf98a7d271837a0e06a79e73847a52c9f1cfb9e4
Kirill Sidorov <kirill.sidorov.94@bk.ru>
2018-04-12 00:21:50 +0300

    TC

--- query.js
+++ query.js
@@ -1,10 +1,10 @@
 $('.single-slide').slick({
   dots: true,
   arrows: false,
-  autoplay:false,
+  autoplay:true,
   infinite: true,
-  speed: 200,
-  fade: false,
+  speed: 1000,
+  fade: true,
   customPaging : function(slider, i) {
     var thumb = $(slider.$slides[i]).data();
     return '<a>'+ ('0'+(i+1))+'</a>';
@@ -19,4 +19,27 @@ $(window).scroll(function(){
         $('.headmenu').removeClass('scroll')
     }
 });
+
+$(document).ready(function() {
+  $('.burger').click(function() { // нажимаем на класс .burger 
+    $('.burger').removeClass('active');// удаляем у класса .burger класс active
+    if ($(this).next('.over').css("display") == "none") { // если следующий класс после .burger имеет display = none
+      $('.over').hide('slow');// cкрываем .over
+      $(this).next('.over').toggle('normal');
+      $('.burger').removeClass('active');// удаляем класс
+      $(this).toggleClass('active');//добавляем класс .active
+    } else $('.over').hide('normal');
+    return false;
+  });
+});
+window.onload = function() {
+  Particles.init({
+    selector: '.background',
+	color: '#ffffff',
+	sizeVariations:1,
+	connectParticles:true,
+	maxParticles:25
+  });
+};
+
 
